5dd44ca4b4987bdbfd5393c69d30e6eabdc8f3c2
Chehok <3070311@gmail.com>
2023-12-12 16:39:39 +0900

    .

--- Server/src/main/config/Constant.java
+++
@@ -1,7 +0,0 @@
-package main.config;
-
-public class Constant {
-    public static final String mysqlUrl = "jdbc:mysql://localhost:3306/chatProtocol";
-    public static final String mysqlUser = "";
-    public static final String mysqlPassword = "";
-}
